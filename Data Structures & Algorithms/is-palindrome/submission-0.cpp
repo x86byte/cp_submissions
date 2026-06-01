f61bc459bc38047e0f0f26a1da0e0e507763aca6
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-01 15:58:23 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s)
+    {
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] >= 'A' && s[i] <= 'Z')
+                s[i] += 32;
+        }
+        set<char> se;
+        for(auto c : s)
+            if(isalpha(c))
+            {
+                cout << c << endl;
+                if(!se.insert(c).second)
+                    se.erase(c);
+            }
+        int sez = se.size();
+        if(sez == 0 || sez == 1)
+            return 1;
+        return 0;
+    }
+};
