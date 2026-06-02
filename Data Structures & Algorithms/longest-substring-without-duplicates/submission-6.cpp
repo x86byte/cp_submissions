3a7b22a89e8da52823a6cb2ec1b52b08948bcd57
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-02 20:21:23 +0100

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int fq[256] = {};
+    int lengthOfLongestSubstring(string s)
+    {
+        if(s.empty())
+            return 0;
+        vector<int> re;
+        int ctr = 0;
+        for(int i = 0; i < s.size();)
+        {
+            unsigned char idx = static_cast<unsigned char>(s[i]);
+            if(fq[idx]){
+                cout <<"repeted : " << s[i] << endl;
+                fill(std::begin(fq), std::end(fq), 0);
+                cout << "ctr : " << ctr << endl;
+                re.push_back(ctr);
+                ctr=0;
+                continue;
+            }
+            ctr++;
+            cout << "fresh : " << s[i] << endl;
+                cout << "ctr : " << ctr << endl;
+            fq[idx]++;
+            if(i == s.size()-1)
+            {
+                re.push_back(ctr);
+                break;
+            }
+            i++;
+        }
+        return *(max_element(re.begin(), re.end()));
+    }
+};
