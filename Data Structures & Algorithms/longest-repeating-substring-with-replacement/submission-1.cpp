3ec36a3c55d54e9548e0432865464708cbcf71b8
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-03 15:51:02 +0100

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k)
+    {
+        int st = 0;
+        int en = s.size() - 1;
+        vector<int> re;
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            int v = 0;
+            int ctr = 0;
+            string t = s;
+            st = i;
+            char c = s[st];
+
+            for(int d = en; d >= 0;)
+            {
+                if(s[d] == c)
+                {
+                    ctr = 1;
+                    while(st <= d)
+                    {
+                        ctr++;
+                        if(t[st] != c)
+                        {
+                            t[st] = c;
+                            v++;
+                        }
+                        st++;
+                        if(v == k)
+                            break;
+                    }
+                    re.push_back(ctr);
+                    break;
+                }
+                d--;
+            }
+        }
+        return *(max_element(re.begin(), re.end()));
+    }
+};
