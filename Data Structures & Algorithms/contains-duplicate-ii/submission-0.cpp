1afce41ce74f28b2cc57710f430aba80119cf4af
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-08 18:38:55 +0100

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& n, int k) {
+        for(int i = 0; i <n.size(); i++)
+        {
+            if(i == k)
+                return false;
+            int t = n[i];
+            for(int d = i+1; d <n.size(); d++)
+            {
+                cout << t << " " << n[d] << endl;
+                if(t == n[d])
+                    return true;
+                if(d == k)
+                    return false;
+            }
+        }
+        return false;
+    }
+};
