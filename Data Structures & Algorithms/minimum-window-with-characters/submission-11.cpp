35708fff1bd9a21fd9f2883764d53878888b455b
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-10 16:39:27 +0100

    Add: minimum-window-with-characters - submission-11

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-11.cpp
@@ -0,0 +1,95 @@
+
+
+class Solution {
+public:
+    int checker(string s, set<char> se)
+    {
+        for(auto c : s)
+            for(auto i : se)
+                if(c == i)
+                    return true;
+        return false;
+    }
+    int cf(char f, set<char> se)
+    {
+        for(auto i : se)
+        {
+            if(i == f)
+                return 1;
+        }
+        return 0;
+    }
+    int validator(string s, set<char> se, string r_t)
+    {
+        set<char> sss;
+        for(auto c : s)
+            sss.insert(c);
+        string rep;
+        for(auto c : sss)
+            rep += c;
+        string r_s;
+        for(int i = 0; i < rep.size(); i++)
+        {
+            if(cf(rep[i], se))
+            {
+                r_s += rep[i];
+            }
+        }
+        size_t ctr  = 0;
+        for(auto c : r_s)
+            for(auto i : se)
+                if(c == i){
+                    ctr++;
+                }
+        if(ctr == se.size() && cf(s[0], se))
+            return true;
+        return false;
+    }
+    string minWindow(string s, string t)
+    {
+        if(s==t)
+            return s;
+        if(s.size() < t.size())
+            return "";
+        set<char> r_t;
+        for(auto c : t)
+            r_t.insert(c);
+        vector<string> rr;
+        int l = 0;
+        int rs = s.size();
+        for(int i = 0; i < s.size(); i++)
+        {
+            string re;
+            for(int d = l; d<rs; d++)
+            {
+                re+= s[d];
+            }
+            if(re.size())
+                if(checker(re, r_t) && cf(s[rs-1], r_t))
+                    rr.push_back(re);
+                else{
+                    l = -1;
+                    rs--;
+                }
+            l++;
+        }
+        int min = INT_MAX;
+        string retme;
+        for(auto ch : rr)
+            if(validator(ch, r_t, t))
+            {
+                if(min > ch.size())
+                {
+                    min  = ch.size();
+                    retme = ch;
+                    cout << retme << endl;
+                }
+            }
+        if(s.size() == t.size())
+        {
+            if(retme.size() < t.size())
+                return "";
+        }
+        return retme;
+    }
+};
