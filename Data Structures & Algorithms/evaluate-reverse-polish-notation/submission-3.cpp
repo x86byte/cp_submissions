3426cf384f071b7396b19b8e1be9715333cdb145
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-10 19:28:23 +0100

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int is_token(string tn)
+    {
+        if(tn == "+" || tn == "-" || tn == "/" || tn == "*")
+            return 1;
+        return 0;
+    }
+    int evalRPN(vector<string>& t) {
+        stack<int> stck;
+        for(auto e : t)
+        {
+            if(is_token(e))
+            {
+                int f = stck.top(); stck.pop();
+                int s = stck.top(); stck.pop();
+
+                cout << f << " " << s << endl;
+                if(e == "-")
+                {
+                    stck.push(s - f);
+                    continue;
+                }
+                if(e == "+")
+                {
+                    stck.push(s + f);
+                    continue;
+                }
+                if(e == "/")
+                {
+                    stck.push(s / f);
+                    continue;
+                }
+                if(e == "*")
+                {
+                    stck.push(f * s);
+                    continue;
+                }
+            }
+            else
+            {
+                stck.push(stoi(e));
+            }
+        }
+        return stck.top();
+    }
+};
