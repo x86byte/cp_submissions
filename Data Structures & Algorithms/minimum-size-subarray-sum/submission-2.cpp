b5cdff33d90a14ef635bb1e899445cd5d561f173
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-12 16:44:25 +0100

    Add: minimum-size-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& n)
+    {
+        // [2,1,5,1,5,3]
+        // 8, 7, 11, 9
+        if(count(n.begin(), n.end(), target))
+            return 1;
+        vector<int> v;
+        int l =0, r =0, sum  = 0;
+        for(int i = l; i < n.size(); i++)
+        {
+            sum += n[l];
+            if(sum >= target)
+                return 1;
+            cout << n[l]<< " ";
+            for(int d= l+1; d<n.size(); d++)
+            {
+                sum+= n[d];
+                r++;
+                cout << n[d]<< " ";
+                if(sum >= target){
+                    v.push_back(r - l + 1);
+                    r = l +1;
+                    break;
+                }
+            }
+            cout << endl;
+            l++;
+            sum = 0;
+        }
+        if(!v.size())
+            return 0;
+        return *min_element(v.begin(), v.end());
+    }
+};
