ad115dac60c03de137bc188077ba9e1ccbef4598
x86byte <111459558+x86byte@users.noreply.github.com>
2026-06-16 13:50:52 +0100

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& n, int t)
+    {
+        int l = 0;
+        int r = n.size();
+        int m = l+r/2;;
+
+        for(int i = 0;i < n.size(); i++)
+        {
+            if(t >= n[m])
+            {
+                l = m;
+                m = r;
+                while(l<m)
+                {
+                    if(t == n[l])
+                        return l;
+                    l++;
+                }
+            } else
+                while(l<=m)
+                {
+                    if(t == n[l])
+                        return l;
+                    l++;
+                }
+        }
+        return -1;
+    }
+};
